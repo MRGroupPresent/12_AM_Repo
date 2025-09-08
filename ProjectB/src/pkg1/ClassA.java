1e6a4d60fdbea4ce274856ab2e9aa7398f18af62
mreyaz <mreyazqa19@gmail.com>
2025-09-08 15:47:10 +0530

    push

---
+++ ProjectB/src/pkg1/ClassA.java
@@ -0,0 +1,15 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+
+	
+	@Test
+	public void tc1()
+	{
+		System.out.println("Hi");
+		System.out.println("Hello");
+	}
+
+}
